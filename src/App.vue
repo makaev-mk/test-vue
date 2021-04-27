fdce08fa0d09ea805add6a36a5f7cc11414b68a4
m.makaev <m.makaev@alef.im>
2021-04-27 14:30:37 +0300

    lesson 1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container pt-1">
+    <div class="card">
+      <h1>Актульные новости на: {{ dateNews }} </h1>
+    </div>
+    <div class="card mt-2 p-3" v-for="item in news" :key="item">
+      <h3>{{ item }}</h3>
+      <p v-if="isOpen">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos eveniet facere id illo ipsum laboriosam magni, nihil nobis non nulla rem repudiandae sit sunt vel velit voluptatibus. Praesentium, quibusdam.</p>
+      <button class="btn btn-primary" @click="isOpen = !isOpen">Открыть</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      dateNews : new Date().toLocaleDateString(),
+      news: [
+          'Путин призвал избегать популизма во время предвыборной кампании',
+          'Прививка от пневмококка может облегчить течение коронавируса',
+          'Официальный курс евро на среду вырос на две копейки'
+      ],
+      isOpen: false
+    }
+  },
+  methods: {
+    open() {
+      this.isOpen = !this.isOpen
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
