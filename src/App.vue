d3e314e88d85f976d125a35fb647aa5e01e3fcff
m.makaev <m.makaev@alef.im>
2021-04-28 10:12:57 +0300

    emit - valiadtor

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
   <div class="container pt-1">
     <div class="card">
       <h1>Актульные новости на: {{ dateNews }} </h1>
+      <span>Открыто: {{ openRate }} | Прочитано: {{ writeNews }}</span>
     </div>
     <AppNews v-for="item in news"
               :title="item.title"
              :id="item.id"
              :is-open="item.isOpen"
+             :was-read="item.wasRead"
+             @open-news="openRate++"
+             @read-news="readNews"
+             @unmark="unreadNews"
              :key="item.id"
     />
   </div>
@@ -19,30 +24,42 @@ export default {
   name: 'App',
   data() {
     return {
+      writeNews: 0,
+      openRate: 0,
       dateNews : new Date().toLocaleDateString(),
       news: [
         {
           title: 'Официальный курс евро на среду вырос на две копейки',
           id: 1,
-          isOpen:false
+          isOpen:false,
+          wasRead: false
         },
         {
           title: 'Путин рассчитывает на экспертную проработку мер по реализации послания',
           id: 2,
-          isOpen:false
+          isOpen:false,
+          wasRead: false
         },
         {
           title: 'ДНР порекомендовала Киеву отказаться от попыток переписать "Минск-2"',
           id: 3,
-          isOpen:false
+          isOpen:false,
+          wasRead: false
         }
       ]
     }
   },
   methods: {
-    // open() {
-    //   this.isOpen = !this.isOpen
-    // }
+    readNews(data) {
+      const idx = this.news.findIndex(news=> news.id === data)
+      this.news[idx].wasRead = true
+      this.writeNews++
+    },
+    unreadNews(data) {
+      const news = this.news.find(news=>news.id === data)
+      news.wasRead = false
+      this.writeNews--
+    }
   },
   components: {
     AppNews
--- src/components/AppNews.vue
+++ src/components/AppNews.vue
@@ -2,8 +2,20 @@
   <div>
     <div class="card mt-2 p-3">
       <h3>{{ title }}</h3>
-      <p v-if="openNews">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos eveniet facere id illo ipsum laboriosam magni, nihil nobis non nulla rem repudiandae sit sunt vel velit voluptatibus. Praesentium, quibusdam.</p>
-      <button class="btn" @click="open">Открыть</button>
+      <div class="d-flex m-auto">
+        <button class="btn" @click="open">
+          {{openNews ? 'Закрыть' : 'Открыть'}}
+        </button>
+        <button class="btn btn-danger"
+                v-if="wasRead" @click="$emit('unmark', id)">
+          Отменить прочтение
+        </button>
+      </div>
+      <div v-if="openNews">
+        <hr/>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos eveniet facere id illo ipsum laboriosam magni, nihil nobis non nulla rem repudiandae sit sunt vel velit voluptatibus. Praesentium, quibusdam.</p>
+        <button class="btn btn-primary m-3" v-if="!wasRead" @click="mark">Прочесть новость</button>
+      </div>
     </div>
   </div>
 </template>
@@ -11,7 +23,14 @@
 <script>
 export default {
   // props: ['title'],
+  // emits: ['open-news'],
+  emits: {
+    'open-news': null,
+    'read-news': null,
+    'unmark': null
+  },
   props: {
+    wasRead: Boolean,
     title: {
       type: String,
       required:true
@@ -24,7 +43,7 @@ export default {
       type: Boolean,
       required: false,
       default: false
-    }
+    },
   },
   data() {
     return {
@@ -34,7 +53,17 @@ export default {
   methods: {
     open() {
       this.openNews = !this.openNews
-    }
+      if (this.openNews) {
+        this.$emit('open-news')
+      }
+    },
+    mark() {
+      this.openNews = false
+      this.$emit('read-news', this.id)
+    },
+    // unmark() {
+    //   this.$emit('unmark', this.id)
+    // }
   }
 }
 </script>
